535e05d192c37aeb0775f083078d4aa3d9ff8858
abhishekkumar085 <abhishek950820@gmail.com>
2024-08-11 11:59:12 +0530

    Added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home'
+// import { CurrencyContext } from './context/CurrencyContext';
+
+function App() {
+
+  // const [currency, setCurrency] = useState('usd');
+
+  return (
+    <>
+      {/* <CurrencyContext.Provider value={ { currency, setCurrency } }> */}
+        <Home />
+      {/* </CurrencyContext.Provider> */}
+    </>
+  )
+}
+
+export default App
